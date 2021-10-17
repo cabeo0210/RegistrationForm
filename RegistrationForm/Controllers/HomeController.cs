ed9a441a63412aa243beed758c7a8e8532f02c09
cabeo0210 <cabeo0210@gmail.com>
2021-10-17 22:03:15 +0700

    Add project files.

---
+++ RegistrationForm/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RegistrationForm.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistrationForm.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        public IActionResult Index()
+        {
+            return View("myView");
+        }
+
+        public IActionResult Register() {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(Student student) {
+            if (ModelState.IsValid)
+            {
+                Repository.AddStudent(student);
+                return View("Thanks", student);
+            }
+            else {
+                return View();
+            }
+        }
+        public IActionResult StudentList() {
+            return View(Repository.GetStudents());
+        }
+    }
+}
---
+++ RegistrationForm/Models/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistrationForm.Models
+{
+    public class Student
+    {
+        private String name;
+        private String email;
+        private String phone;
+        private bool hasGraduate;
+
+        [Required(ErrorMessage = "Plese Enter Your Name!")]
+        public string Name { get => name; set => name = value; }
+
+        [Required(ErrorMessage = "Plese Enter Your Email!")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get => email; set => email = value; }
+
+        [Required(ErrorMessage = "Plese Enter Your Phone!")]
+
+        public string Phone { get => phone; set => phone = value; }
+        [Required(ErrorMessage = "Plese choose if you are graduate or not")]
+
+        public bool? HasGraduate { get; set; }
+    }
+}
